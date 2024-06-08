ea1e0d01313549f4db3667dae683b392c58c4308
AnitaKamani <AnitaKamani@outlook.com>
2024-06-08 17:27:15 +0330

    bagher mashgh done

--- 08_mashghe_bagher/src/main.rs
+++ 08_mashghe_bagher/src/main.rs
@@ -3,5 +3,19 @@ use std::io;
 fn main() {
     let mut input = String::new();
     _ = io::stdin().read_line(&mut input).expect("wrong answer");
-    println!("{}", input);
+    let inputs: Vec<usize> = input
+        .trim()
+        .split(" ")
+        .map(|x| x.parse().expect("Not an intiger"))
+        .collect();
+    let sum: usize = inputs.iter().sum();
+
+    let all_greather_than_zero: bool = inputs.iter().all(|&x| x > 0);
+    let sum_check: bool = sum == 180;
+
+    if all_greather_than_zero && sum_check {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
