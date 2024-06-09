0114c41833c69d02c9caa6db22a5b4799698326c
AnitaKamani <AnitaKamani@outlook.com>
2024-06-09 11:09:42 +0330

    ro be ro done

--- 02_ro_be_ro_dar_metro/src/main.rs
+++ 02_ro_be_ro_dar_metro/src/main.rs
@@ -1,13 +1,27 @@
 use std::io;
-use std::io::prelude::*;
 
 fn main() {
-    let mut v = Vec::new();
-    for x in 0..2 {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input);
+    let mut input_1 = String::new();
+    let mut input_2 = String::new();
+    _ = io::stdin().read_line(&mut input_1).expect("wrong answer");
+    _ = io::stdin().read_line(&mut input_2).expect("wrong answer");
 
-        v.push(input);
-    }
-    // println!("{v}");
+    let input_1_vec: Vec<usize> = input_1
+        .trim()
+        .split(" ")
+        .map(|x| x.parse().expect("Not an intiger"))
+        .collect();
+    let input_2_vec: Vec<usize> = input_2
+        .trim()
+        .split(" ")
+        .map(|x| x.parse().expect("Not an intiger"))
+        .collect();
+
+    let ro_be_ro: usize = input_1_vec
+        .iter()
+        .zip(input_2_vec.iter())
+        .map(|(x, y)| x * y)
+        .sum();
+
+    println!("{}", ro_be_ro);
 }
